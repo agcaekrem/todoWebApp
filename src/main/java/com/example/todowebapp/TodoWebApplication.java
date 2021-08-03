7ce78c599909c5c1ca48a97263bdb7d2279b039e
agcaekrem <agcaa.ekrem@gmail.com>
2021-08-04 01:21:55 +0300

    mail

--- src/main/java/com/example/todowebapp/TodoWebApplication.java
+++ src/main/java/com/example/todowebapp/TodoWebApplication.java
@@ -3,10 +3,9 @@ package com.example.todowebapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication (exclude = {DataSourceAutoConfiguration.class})
-@Component
+
 public class TodoWebApplication {
 
     public static void main(String[] args) {
